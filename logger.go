7368e68ff9d7fd6a31cea8a780e89fc66ea71db7
Mikhail Brel <m@brel.me>
2025-05-10 12:17:21 -0400

    with caller

--- logger.go
+++ logger.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -31,7 +32,7 @@ var (
 type Config struct {
 	Level      string    // Log level: debug, info, warn, error, fatal, panic
 	Pretty     bool      // Enable pretty (human-readable) logging
-	WithCaller bool      // Include caller information in logs
+	WithCaller bool      // Include caller information in logs as a custom field
 	TimeFormat string    // Timestamp format
 	Output     io.Writer // Output writer (defaults to stderr)
 }
@@ -86,10 +87,9 @@ func InitLogger(cfg Config) {
 		// Add timestamp to all logs
 		logger = logger.With().Timestamp().Logger()
 
-		// Optionally add caller info
-		if cfg.WithCaller {
-			logger = logger.With().Caller().Logger()
-		}
+		// Store caller setting in defaultConfig for use in log methods
+		// We'll handle caller differently by adding a custom field
+		defaultConfig.WithCaller = cfg.WithCaller
 
 		// Set both our package-level DefaultLogger and zerolog's global logger
 		// This ensures ALL code using either one will get the same configuration
@@ -101,98 +101,88 @@ func InitLogger(cfg Config) {
 // GetLogger returns a contextualized logger with the component field set
 // This is useful for identifying which module generated a log entry
 func GetLogger(component string) zerolog.Logger {
+	// Add component information and caller if enabled
+	context := DefaultLogger.With()
+	context = addCallerToContext(context).Str("component", component)
+	return context.Logger()
+}
 
-	return DefaultLogger.With().Str("component", component).Logger()
+// addCallerInfo adds caller information to the event if WithCaller is enabled
+// skipFrames is the number of additional stack frames to skip (1 for this function)
+func addCallerInfo(evt *zerolog.Event) *zerolog.Event {
+	if defaultConfig.WithCaller {
+		// Get the caller's location (skipping the wrapper function)
+		_, file, line, ok := runtime.Caller(2) // Skip this function + caller
+		if ok {
+			evt = evt.Str("caller", fmt.Sprintf("%s:%d", file, line))
+		}
+	}
+	return evt
+}
+
+// addCallerToContext adds caller information to the context if WithCaller is enabled
+func addCallerToContext(ctx zerolog.Context) zerolog.Context {
+	if defaultConfig.WithCaller {
+		// Get the caller's location (skipping the wrapper function)
+		_, file, line, ok := runtime.Caller(2) // Skip this function + caller
+		if ok {
+			ctx = ctx.Str("caller", fmt.Sprintf("%s:%d", file, line))
+		}
+	}
+	return ctx
+}
+
+// processArgs handles the different argument formats for log messages
+func processArgs(evt *zerolog.Event, msg string, args ...interface{}) {
+	if len(args) > 0 && strings.Contains(msg, "%") {
+		evt.Msgf(msg, args...)
+	} else if len(args) > 0 {
+		for i := 0; i < len(args); i += 2 {
+			if i+1 < len(args) {
+				evt = evt.Interface(fmt.Sprint(args[i]), args[i+1])
+			}
+		}
+		evt.Msg(msg)
+	} else {
+		evt.Msg(msg)
+	}
 }
 
 // Debug logs a debug message
 func Debug(msg string, args ...interface{}) {
-	if len(args) > 0 && strings.Contains(msg, "%") {
-		DefaultLogger.Debug().Msgf(msg, args...)
-	} else if len(args) > 0 {
-		evt := DefaultLogger.Debug()
-		for i := 0; i < len(args); i += 2 {
-			if i+1 < len(args) {
-				evt = evt.Interface(fmt.Sprint(args[i]), args[i+1])
-			}
-		}
-		evt.Msg(msg)
-	} else {
-		DefaultLogger.Debug().Msg(msg)
-	}
+	evt := addCallerInfo(DefaultLogger.Debug())
+	processArgs(evt, msg, args...)
 }
 
 // Info logs an info message
 func Info(msg string, args ...interface{}) {
-	if len(args) > 0 && strings.Contains(msg, "%") {
-		DefaultLogger.Info().Msgf(msg, args...)
-	} else if len(args) > 0 {
-		evt := DefaultLogger.Info()
-		for i := 0; i < len(args); i += 2 {
-			if i+1 < len(args) {
-				evt = evt.Interface(fmt.Sprint(args[i]), args[i+1])
-			}
-		}
-		evt.Msg(msg)
-	} else {
-		DefaultLogger.Info().Msg(msg)
-	}
+	evt := addCallerInfo(DefaultLogger.Info())
+	processArgs(evt, msg, args...)
 }
 
 // Warn logs a warning message
 func Warn(msg string, args ...interface{}) {
-	if len(args) > 0 && strings.Contains(msg, "%") {
-		DefaultLogger.Warn().Msgf(msg, args...)
-	} else if len(args) > 0 {
-		evt := DefaultLogger.Warn()
-		for i := 0; i < len(args); i += 2 {
-			if i+1 < len(args) {
-				evt = evt.Interface(fmt.Sprint(args[i]), args[i+1])
-			}
-		}
-		evt.Msg(msg)
-	} else {
-		DefaultLogger.Warn().Msg(msg)
-	}
+	evt := addCallerInfo(DefaultLogger.Warn())
+	processArgs(evt, msg, args...)
 }
 
 // Error logs an error message
 func Error(err error, msg string, args ...interface{}) {
-	event := DefaultLogger.Error().Err(err)
-	if len(args) > 0 && strings.Contains(msg, "%") {
-		event.Msgf(msg, args...)
-	} else if len(args) > 0 {
-		for i := 0; i < len(args); i += 2 {
-			if i+1 < len(args) {
-				event = event.Interface(fmt.Sprint(args[i]), args[i+1])
-			}
-		}
-		event.Msg(msg)
-	} else {
-		event.Msg(msg)
-	}
+	evt := addCallerInfo(DefaultLogger.Error().Err(err))
+	processArgs(evt, msg, args...)
 }
 
 // Fatal logs a fatal message and exits
 func Fatal(err error, msg string, args ...interface{}) {
-	event := DefaultLogger.Fatal().Err(err)
-	if len(args) > 0 && strings.Contains(msg, "%") {
-		event.Msgf(msg, args...)
-	} else if len(args) > 0 {
-		for i := 0; i < len(args); i += 2 {
-			if i+1 < len(args) {
-				event = event.Interface(fmt.Sprint(args[i]), args[i+1])
-			}
-		}
-		event.Msg(msg)
-	} else {
-		event.Msg(msg)
-	}
+	evt := addCallerInfo(DefaultLogger.Fatal().Err(err))
+	processArgs(evt, msg, args...)
 }
 
 // WithField adds a field to the logger context
 func WithField(key string, value interface{}) zerolog.Logger {
-	return DefaultLogger.With().Interface(key, value).Logger()
+	context := DefaultLogger.With()
+	context = addCallerToContext(context).Interface(key, value)
+	return context.Logger()
 }
 
 // FormatError creates a formatted error string
